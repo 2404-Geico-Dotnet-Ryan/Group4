996eeeb1d47d538673a1b56dc68b323b8d556c81
KatPiper1983 <katmacrock@hotmail.com>
2024-06-06 20:53:20 -0400

    a couple of small changes

--- DTO/DTO.cs
+++ DTO/DTO.cs
@@ -3,15 +3,15 @@ namespace Project2.DTO
     public class UserDTO
     {
 
-        public int UserId { get; set; } 
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string FirstName { get; set; }   
-        public string LastName { get; set; }
-        public int MaxBudget { get; set; }        
+    public int UserId { get; set; } 
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string FirstName { get; set; }   
+    public string LastName { get; set; }
+    public int MaxBudget { get; set; }        
     }
 
-        public class SavedTripDTO
+    public class SavedTripDTO
     {
     public int TripId { get; set; }
     public int UId { get; set; } 
@@ -23,7 +23,7 @@ namespace Project2.DTO
     public string? TravelType { get; set; }
     public string? ClimatePref { get; set; }
     public bool PassportStatus { get; set; }
-    public string? IncludedActivities { get; set; }
+    public string? ActivityName { get; set; }
     }
 
     public class TripDTO
@@ -34,7 +34,7 @@ namespace Project2.DTO
     public string? TravelType { get; set; }
     public string? Climate { get; set; }
     public bool NeedsPassport { get; set; }
-    public string? IncludedActivities { get; set; }
+    public string? ActivityName { get; set; }
 
     }
 
